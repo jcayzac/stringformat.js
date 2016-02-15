15a26ae2b794e96e72077bb63e7f158f5f6e93ba
mbredif <mathieu.bredif@ign.fr>
2016-02-15 09:20:44 +0100

    Non matching accolade fix (test fix)

--- test.js
+++ test.js
@@ -123,7 +123,7 @@ module.exports = {
 		assert.equal($("{0}}}", "two"), "two}}")
 	},
 	'test with non-matching accolades 2': function() {
-		assert.equal($("{{{0}}", "two"), "{two")
+		assert.equal($("{{{0}", "two"), "{two")
 	},
 	'test with non-matching accolades, issue #1': function() {
 		assert.equal($("{1{test}}", { test: 1 }), "{11}")
